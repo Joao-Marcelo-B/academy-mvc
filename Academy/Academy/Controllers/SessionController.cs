0ad37cd6ac9b7ef928cac2e9a6aa806e2d67e8c4
João Marcelo <joaomarcelobatistanarciso@gmail.com>
2024-05-19 13:02:58 -0300

    f

--- Academy/Academy/Controllers/SessionController.cs
+++ Academy/Academy/Controllers/SessionController.cs
@@ -18,7 +18,37 @@ namespace Academy.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create()
+        public IActionResult Create(Usuario usuario)
+        {
+            usuarios.Add(usuario);
+            usuario.UsuarioId = usuarios.Select(u => u.UsuarioId).Max() + 1;
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            var confirma = usuarios.Where(u => u.Email.Equals(email) && u.Senha.Equals(senha)).FirstOrDefault();
+            if(confirma != null)
+            {
+                HttpContext.Session.SetString("usuario_session", confirma.Nome);
+                return RedirectToAction("Correto");
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        public IActionResult Correto()
+        {
+            return View();
+        }
     }
 }
  
