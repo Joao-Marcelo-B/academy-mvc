8448be0d132bdbb8295aed904dce9302d3b73d73
Joao-Marcelo-WMoney <joao.narciso@wealthmoney.com.br>
2024-05-19 12:32:38 -0300

    pronto

---
+++ Academy/Academy/Controllers/SessionController.cs
@@ -0,0 +1,24 @@
+﻿using Academy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academy.Controllers
+{
+    public class SessionController : Controller
+    {
+        private static List<Usuario> usuarios = new();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+    }
+}
+ 
